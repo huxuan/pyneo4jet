411bd9319e24afe0afffad3a7de37f978757919d
huxuan <i@huxuan.org>
2012-11-25 13:16:48 +0800

    pyneo4jet.py:
    - Add routes basic test
    - Bug fix for variables in routes
    - Add index variable for tweet_get and friends_get

--- pyneo4jet.py
+++ pyneo4jet.py
@@ -30,14 +30,14 @@ def login_or_timeline_get():
     show the timeline if is
     show login otherwise
     """
-    pass
+    return 'GET /'
 
 @post('/')
 def login_post():
     """
     Check whether post the corresponding username and password
     """
-    pass
+    return 'POST /'
 
 @get('/register/')
 def register_get():
@@ -47,7 +47,7 @@ def register_get():
     Note:
         Currently INVITATION_CODE is NEEDED!
     """
-    pass
+    return 'GET /register/'
 
 @post('/register/')
 def register_post():
@@ -58,49 +58,51 @@ def register_post():
         Need to check whether username, avatar and INVITATION_CODE is valid
         avatar could be optional but should have default value
     """
-    pass
+    return 'POST /register/'
 
 @get('/profile/')
 def profile_get():
     """
     Show form of profile with subbmit button which can change it.
     """
-    pass
+    return 'GET /profile/'
 
 @post('/profile/')
 def profile_post():
     """
     Update profile and redirect to get page
     """
-    pass
+    return 'POST /profile/'
 
 @get('/tweet/')
-def tweet_get():
+@get('/tweet/<index:int>')
+def tweet_get(index=0):
     """
     Show an empty form for tweet
     """
-    pass
+    return 'GET /tweet/%d' % index
 
 @post('/tweet/')
 def tweet_post():
     """
     Add a new tweet
     """
-    pass
+    return 'POST /tweet/'
 
 @get('/user/<username>')
-def user_get():
+def user_get(username):
     """
     Show user's profile and tweets as well as follow link or followed status.
     """
-    pass
+    return 'GET /user/%s' % username
 
 @get('/user/<username>/friends/')
-def user_firends_get():
+@get('/user/<username>/friends/<index:int>')
+def user_firends_get(username, index=0):
     """
     Show list for user's friends
     """
-    pass
+    return 'GET /user/%s/friends/%d' % (username, index, )
 
 def main():
     """Parse the args and run the server"""
