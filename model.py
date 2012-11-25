17e215b5b51cf36c8b3a204275af0a87ca309e62
huxuan <i@huxuan.org>
2012-11-25 13:52:08 +0800

    Reconstruction of model
    - Merge model.user and model.tweet to model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ Author: huxuan - i(at)huxuan.org
 Created: 2012-11-25
 Last modified: 2012-11-25
 Description:
-    Description for user.py
+    models used in pyneo4jet
 
 Copyrgiht (c) 2012 by huxuan. All rights reserved.
 License GPLv3
@@ -88,3 +88,16 @@ class User(object):
         :rtype: list of tweet instances
         """
         pass
+
+class Tweet(object):
+    """Wrap of all actions related to Tweet
+
+    :param text: the text of tweet
+    :type text: string
+    :param created_at: the time created the tweet
+    :type created_at: datetime
+    """
+    def __init__(self, text, created_at):
+        """Init Tweet"""
+        self.text = text
+        self.created_at = created_at
