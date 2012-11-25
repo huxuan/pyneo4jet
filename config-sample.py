1adcde73d466739016c6f99bef942ba334e97527
huxuan <i@huxuan.org>
2012-11-25 10:11:32 +0800

    Add initial version of config-sample.py

---
+++ config-sample.py
@@ -0,0 +1,19 @@
+"""
+File: config-sample.py
+Author: huxuan - i(at)huxuan.org
+Created: 2012-11-25
+Last modified: 2012-11-25
+Description:
+    Sample for config.py
+    Mostly for some global variables
+
+Copyrgiht (c) 2012 by huxuan. All rights reserved.
+License GPLv3
+"""
+
+# VERSION should be development or production to judge
+# whether to deploy for test or not
+VERSION = 'development | production'
+
+# INVITATION_CODE is needed currently for register
+INVITATION_CODE = 'whatever_you_want'
