e9026e21e4542939bdc2211f1ef235bd461e954d
huxuan <i@huxuan.org>
2012-11-25 13:33:23 +0800

    Add __init__.py for model to make it importable

---
+++ model/__init__.py
@@ -0,0 +1,11 @@
+"""
+File: __init__.py
+Author: huxuan - i(at)huxuan.org
+Created: 2012-11-25
+Last modified: 2012-11-25
+Description:
+    Make model importable
+
+Copyrgiht (c) 2012 by huxuan. All rights reserved.
+License GPLv3
+"""
