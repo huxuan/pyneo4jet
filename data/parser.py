4883253b7bfeae74f2fde6f3505611f67ec5ce60
huxuan <i@huxuan.org>
2012-12-10 13:52:26 +0800

    data-parser should work now

--- data/parser.py
+++ data/parser.py
@@ -14,7 +14,8 @@ License GPLv3
 
 import re
 
-from model import User
+from model import User, db
+from config import INVITATION_CODE
 
 INFO_PATTERN = re.compile('"([^"]+)"')
 
@@ -29,12 +30,23 @@ def main():
 
     data = file('data/petster-hamster/ent.petster-hamster')
     for item in data.readlines():
+        item = item.decode('utf8')
+        # print repr(item)
+        # print type(item)
+        # print item
         if not item.startswith('%'):
             res = INFO_PATTERN.findall(item)
             info = dict(zip(KEYS, res))
+            print info['id']
             username_list[info['id']] = info['username']
-            user = User(info)
-            user.add()
+            User.add(
+                username=info['username'],
+                password='pyneo4jet%s' % info['username'],
+                password_confirm='pyneo4jet%s' % info['username'],
+                invitation=INVITATION_CODE,
+            )
+            user = User.get(info['username'])
+            user.update()
     data.close()
 
     data = file('data/petster-hamster/out.petster-hamster')
@@ -43,15 +55,18 @@ def main():
         # raw_input()
         if not item.startswith('%'):
             uid1, uid2 = item.strip().split(' ')
-            # print repr(uid1), repr(uid2)
+            print repr(uid1), repr(uid2)
             # raw_input()
-            username1 = username_list[uid1]
-            username2 = username_list[uid2]
-            user1 = User.get(username1)
-            user2 = User.get(username2)
-            user1.follow(username2)
-            user2.follow(username1)
+            username1 = username_list.get(uid1)
+            username2 = username_list.get(uid2)
+            if username1 and username2:
+                user1 = User.get(username1)
+                user2 = User.get(username2)
+                user1.follow(username2)
+                user2.follow(username1)
     data.close()
+
+    db.shutdown()
 
 if __name__ == '__main__':
     main()
